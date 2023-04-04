12e6374c1e2c17bc8132a24cab4bc33969473b8b
brijesh9509 <jsk143fams@gmail.com>
2023-04-05 00:54:51 +0530

    v1.0.2

--- app/src/main/jni/api_config.cpp
+++ app/src/main/jni/api_config.cpp
@@ -1,14 +1,7 @@
 #include <jni.h>
 #include <string>
 
-/*std::string SERVER_URL = "https://moodtv.in/ALL_SERVER/HSLIVE/1/be/rest-api/";
-std::string API_KEY = "0e73a6a2b6ec22ab250db32e";*/
-
 std::string SERVER_URL = "https://hotsutra.live/phpadmin/rest-api"; //LIVE
-//std::string API_KEY = "da64cb369c5bdf300750fa95";  //LIVE
-
-/*std::string SERVER_URL = "http://phpstack-781815-2699502.cloudwaysapps.com/phpadmin/rest-api"; //Test
-std::string API_KEY = "da64cb369c5bdf300750fa95";  //Test*/
 
 std::string PURCHASE_CODE = "xxxxxxxxxxxxxxx";
 std::string ONESIGNAL_APP_ID = "ca0b7d1c-da39-46fe-917b-922bc4159fc5";
